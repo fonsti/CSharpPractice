ad04804a63141a39ff4ca2400958b6dc373c6690
DESKTOP-Q66HF8U\timal <tim.fonshell@gmail.com>
2022-08-22 12:55:46 +0200

    Added sqlite connection to save contacts

--- ContactsApp/DesktopContacsApp/DesktopContacsApp/Classes/Contact.cs
+++ ContactsApp/DesktopContacsApp/DesktopContacsApp/Classes/Contact.cs
@@ -11,7 +11,7 @@ namespace DesktopContacsApp.Classes
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-
+        
         public string Name { get; set; }
 
         public string Email { get; set; }
--- ContactsApp/DesktopContacsApp/DesktopContacsApp/NewContactWindow.xaml.cs
+++ ContactsApp/DesktopContacsApp/DesktopContacsApp/NewContactWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using DesktopContacsApp.Classes;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -9,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SQLite;
 
 namespace DesktopContacsApp
 {
@@ -24,6 +26,25 @@ namespace DesktopContacsApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Contact newContact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+
+            string dbName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath, dbName);
+
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+
+            connection.CreateTable<Contact>();
+
+            connection.Insert(newContact);
+
+            connection.Close();
+
             this.Close();
         }
     }
