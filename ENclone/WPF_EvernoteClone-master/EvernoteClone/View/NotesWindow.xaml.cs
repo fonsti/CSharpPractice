7328984391f56466e377c04c83f72dcecfcdddd4
DESKTOP-Q66HF8U\timal <tim.fonshell@gmail.com>
2022-09-05 13:54:58 +0200

    Added italic, underline, font family, and font size to notes.

--- ENclone/WPF_EvernoteClone-master/EvernoteClone/View/NotesWindow.xaml.cs
+++ ENclone/WPF_EvernoteClone-master/EvernoteClone/View/NotesWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +22,15 @@ namespace EvernoteClone.View
         public NotesWindow()
         {
             InitializeComponent();
+
+            var fontFamilies = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            FontFamilyComboBox.ItemsSource = fontFamilies;
+
+            List<double> fontSizes = new List<double>()
+            {
+                8, 9, 10, 11, 12, 14, 16, 28, 48, 72
+            };
+            FontSizeComboBox.ItemsSource = fontSizes;
         }
 
         private void ContentRichtTextbox_TextChanged(object sender, TextChangedEventArgs e)
@@ -44,10 +54,62 @@ namespace EvernoteClone.View
             }
         }
 
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isButtonChecked = (sender as ToggleButton).IsChecked ?? false;
+
+            if (isButtonChecked)
+            {
+                ContentRichtTextbox.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Italic);
+            }
+            else
+            {
+                ContentRichtTextbox.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Normal);
+            }
+        }
+
+        private void UnderlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isButtonChecked = (sender as ToggleButton).IsChecked ?? false;
+
+            if (isButtonChecked)
+            {
+                ContentRichtTextbox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+            }
+            else
+            {
+                TextDecorationCollection textDecorations;
+                (ContentRichtTextbox.Selection.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection).TryRemove(TextDecorations.Underline, out textDecorations);
+                ContentRichtTextbox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, textDecorations);
+            }
+        }
+
         private void ContentRichtTextbox_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            var selectedWeight = ContentRichtTextbox.Selection.GetPropertyValue(FontWeightProperty);
+            var selectedWeight = ContentRichtTextbox.Selection.GetPropertyValue(Inline.FontWeightProperty);
             BoldButton.IsChecked = (selectedWeight != DependencyProperty.UnsetValue) && selectedWeight.Equals(FontWeights.Bold);
+
+            var selectedStyle = ContentRichtTextbox.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            ItalicButton.IsChecked = (selectedStyle != DependencyProperty.UnsetValue) && selectedStyle.Equals(FontStyles.Italic);
+
+            var selectedDecoration = ContentRichtTextbox.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            UnderlineButton.IsChecked = (selectedDecoration != DependencyProperty.UnsetValue) && selectedDecoration.Equals(TextDecorations.Underline);
+
+            FontFamilyComboBox.SelectedItem = ContentRichtTextbox.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            FontSizeComboBox.Text = (ContentRichtTextbox.Selection.GetPropertyValue(Inline.FontSizeProperty)).ToString();
+        }
+
+        private void FontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FontFamilyComboBox.SelectedItem != null)
+            {
+                ContentRichtTextbox.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, FontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void FontSizeComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ContentRichtTextbox.Selection.ApplyPropertyValue(Inline.FontSizeProperty, FontSizeComboBox.Text);
         }
     }
 }
