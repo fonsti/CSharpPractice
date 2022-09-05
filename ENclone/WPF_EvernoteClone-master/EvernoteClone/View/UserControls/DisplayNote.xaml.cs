34489ed22d0df07da46abc4c30096dc1a9d0f100
DESKTOP-Q66HF8U\timal <tim.fonshell@gmail.com>
2022-09-05 16:41:32 +0200

    Added custom controls and comboboxes for fontfamily and fontsize

---
+++ ENclone/WPF_EvernoteClone-master/EvernoteClone/View/UserControls/DisplayNote.xaml.cs
@@ -0,0 +1,47 @@
+﻿using EvernoteClone.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EvernoteClone.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayNote.xaml
+    /// </summary>
+    public partial class DisplayNote : UserControl
+    {
+        public Note MyNote
+        {
+            get { return (Note)GetValue(MyNoteProperty); }
+            set { SetValue(MyNoteProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyNote.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyNoteProperty =
+            DependencyProperty.Register("MyNote", typeof(Note), typeof(DisplayNote), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DisplayNote noteUserControl = d as DisplayNote;
+
+            if (noteUserControl != null)
+            {
+                noteUserControl.DataContext = noteUserControl.MyNote;
+            }
+        }
+
+        public DisplayNote()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ENclone/WPF_EvernoteClone-master/EvernoteClone/View/UserControls/DisplayNotebook.xaml.cs
@@ -0,0 +1,47 @@
+﻿using EvernoteClone.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EvernoteClone.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayNotebook.xaml
+    /// </summary>
+    public partial class DisplayNotebook : UserControl
+    {
+        public Notebook MyNotebook
+        {
+            get { return (Notebook)GetValue(MyNotebookProperty); }
+            set { SetValue(MyNotebookProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyNotebook.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyNotebookProperty =
+            DependencyProperty.Register("MyNotebook", typeof(Notebook), typeof(DisplayNotebook), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DisplayNotebook notebookUserControl = d as DisplayNotebook;
+
+            if (notebookUserControl != null)
+            {
+                notebookUserControl.DataContext = notebookUserControl.MyNotebook;
+            }
+        }
+
+        public DisplayNotebook()
+        {
+            InitializeComponent();
+        }
+    }
+}
